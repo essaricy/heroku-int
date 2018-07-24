c6f358b4e31481558f77e1a21c990d0121c537c0
Srikanth Ragi <BH03329>
2018-07-24 17:23:16 +0530

    updated.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World!";
+      return "Hello World! This is Heroku & Github integration example created by Srikanth.";
     }
 
     public static void main(String[] args) {
